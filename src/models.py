517968b1e4dd67779931ef9971b9db5d16b3fb9f
levanmamuladze <124797206+levanmamuladze@users.noreply.github.com>
2023-04-30 12:45:06 +0000

    Simple instagram Database

--- src/models.py
+++ src/models.py
@@ -1,29 +1,43 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from datetime import datetime
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+class User(Base):
+    __tablename__ = 'user'
+    user_id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(250))
+    dob = Column(DateTime)
+    last_login_time = Column(DateTime)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Photo(Base):
+    __tablename__ = 'photo'
+    photo_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+    photo_location = Column(String(250), nullable=False)
+    creation_date = Column(DateTime, default=datetime.utcnow)
+
+class Follow(Base):
+    __tablename__ = 'follow'
+    follow_id = Column(Integer, primary_key=True)
+    user_id1 = Column(Integer, ForeignKey('user.user_id'))
+    user_id2 = Column(Integer, ForeignKey('user.user_id'))
+
+class FeedItem(Base):
+    __tablename__ = 'feed_item'
+    feed_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+    content = Column(String(250))
+    photo_id = Column(Integer, ForeignKey('photo.photo_id'))
+    creation_date = Column(DateTime, default=datetime.utcnow)
+    photo = relationship(Photo)
 
     def to_dict(self):
         return {}
@@ -33,5 +47,5 @@ try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem generating the diagram")
     raise e
